e124284513f9be1e59635925d24cc3d55a00c597
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-27 13:44:46 +0545

    commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const link = "http://www.google.com";
-
   return (
-    <div className="App App-header">
-      <h1>Apple Watches</h1>
-      <div className="App-link">{link}</div>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
