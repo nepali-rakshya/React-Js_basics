d01074fecb3701698539bf05fbdb27a699b0f934
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-28 22:37:28 +0545

    commit

--- practice/src/MainContent.js
+++ practice/src/MainContent.js
@@ -3,13 +3,42 @@ import ReactDOM from "react-dom/client";
 
 const image = "./logo192.png";
 
-const content = (
-  <div>
-    <section className="img">
+function Header() {
+  return (
+    <header
+      className="img"
+      style={{
+        marginBottom: "1em",
+      }}
+    >
       <img src={image} alt="react-logo" width="50px" />
-    </section>
-    <main className="main-content">
-      <h1>Fun facts about React</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2022 Nepali development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function MainContent() {
+  return (
+    <main
+      className="main-content"
+      style={{
+        marginBottom: "1em",
+      }}
+    >
+      <h1
+        style={{
+          marginBottom: "0.3em",
+        }}
+      >
+        Fun facts about React
+      </h1>
       <ul>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
@@ -18,8 +47,23 @@ const content = (
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
     </main>
-  </div>
-);
+  );
+}
+
+function Page() {
+  return (
+    <div
+      style={{
+        margin: "3em",
+      }}
+    >
+      <Header />
+      <MainContent />
+
+      <Footer />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(content);
+root.render(<Page />);
