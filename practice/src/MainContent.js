08c042c23132b1aa3bd25e1714e08e004f83e5ac
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-28 17:23:49 +0545

    commit

--- practice/src/MainContent.js
+++ practice/src/MainContent.js
@@ -1,9 +1,25 @@
-const MainContent = () => {
-  return (
-    <div>
-      <h1>I'm learning React</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-export default MainContent;
+const image = "./logo192.png";
+
+const content = (
+  <div>
+    <section className="img">
+      <img src={image} alt="react-logo" width="50px" />
+    </section>
+    <main className="main-content">
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Fackebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(content);
--- practice/src/index.js
+++ practice/src/index.js
@@ -1,5 +1,4 @@
 import "./index.css";
-import App from "./App";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import MainContent from "./MainContent";
@@ -7,5 +6,5 @@ import MainContent from "./MainContent";
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // const element = <h1>Hello, world</h1>;
 // root.render(element);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MainContent />);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<MainContent />);
