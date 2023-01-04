5ea7b8f7d0f2c0055644d31666738ef9ce6cc885
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-04 13:59:55 +0545

    commit

--- props/src/App.js
+++ props/src/App.js
@@ -9,25 +9,25 @@ const App = () => {
       }}
     >
       <Contact
-        img="./img/singing.jpg"
+        img="/img/singing.jpg"
         name="Mr. Humpty"
         phone="+977 9863215562"
         email="mr.humpty@gmail.com"
       />
       <Contact
-        img="./img/singing.jpg"
+        img="/img/singing.jpg"
         name="Mr. Dumpty"
         phone="+977 9863215562"
         email="mr.dumpty@gmail.com"
       />
       <Contact
-        img="./img/singing.jpg"
+        img="/img/singing.jpg"
         name="Mr. Headson"
         phone="+977 9863215562"
         email="mr.headson@gmail.com"
       />
       <Contact
-        img="./img/singing.jpg"
+        img="/img/singing.jpg"
         name="Mr. Glamor"
         phone="+977 9863215562"
         email="mr.glamor@gmail.com"
