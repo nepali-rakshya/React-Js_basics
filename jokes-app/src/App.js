ea597cad8f2abccea27fdc792f4d3790df4dc7c0
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-04 17:23:38 +0545

    commit

--- jokes-app/src/App.js
+++ jokes-app/src/App.js
@@ -5,20 +5,25 @@ function App() {
     <div className="App">
       <h1>JOKES COLLECTION</h1>
       <Jokes
-        // setup="A pun, a play on words, and a limerick walk into a bar."
+        setup="A pun, a play on words, and a limerick walk into a bar."
         punchline="No joke"
+        likes={10}
+        isPun={true}
       />
       <Jokes
         setup="How do mathematicians scold their children?"
         punchline="If I've told you n times, I've told you n+1 times…"
+        likes={10}
       />
       <Jokes
         setup="You're late!” she yells. “You said you'd be home by 11:45!"
         punchline="“Actually,” the mathematician replies coolly, “I said I'd be home by a quarter of 12.”"
+        likes={10}
       />
       <Jokes
-        setup="Did you hear about the mathematician who’s afraid of negative numbers?"
+        setup="Did you hear about the mathematician who's afraid of negative numbers?"
         punchline="He will stop at nothing to avoid them."
+        likes={10}
       />
     </div>
   );
--- jokes-app/src/Jokes.js
+++ jokes-app/src/Jokes.js
@@ -1,8 +1,16 @@
 const Jokes = (props) => {
   return (
     <main style={{ width: "60%", border: "2px solid pink" }}>
-      {props.setup && <h4>Setup: {props.setup}</h4>}
+      {/* {props.setup && <h4>Setup: {props.setup}</h4>} */}
+      {/* <h4 style={{ display: props.setup ? "block" : "none" }}>
+        Setup: {props.setup}
+      </h4> */}
+      {props.setup && (
+        <h4 style={{ background: "lightpink" }}>Setup: {props.setup}</h4>
+      )}
       <p>Punchline: {props.punchline}</p>
+      <p>No of likes: {props.likes}</p>
+      <p>It is pun: {props.isPun}</p>
     </main>
   );
 };
