252b79345769e8790eb6ea9631631d532888496c
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-08 23:00:44 +0545

    commit

--- non-static-web/src/App.js
+++ non-static-web/src/App.js
@@ -1,7 +1,20 @@
 import "./index.css";
 
 function App() {
-  return <div></div>;
+  function handleHover() {
+    console.log("I am a react logo");
+  }
+
+  function onMouseOver() {
+    console.log("Mouse over");
+  }
+
+  return (
+    <div>
+      <img src="/logo192.png" alt="" onMouseOver={onMouseOver} /> <br />
+      <button>Click me</button>
+    </div>
+  );
 }
 
 export default App;
