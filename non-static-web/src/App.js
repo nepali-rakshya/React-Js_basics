2b6619cffa2c00916e92d72d5488eb9d149b80f8
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-10 14:20:06 +0545

    commit

--- non-static-web/src/App.js
+++ non-static-web/src/App.js
@@ -12,9 +12,25 @@ function App() {
     console.log(thingsArray);
   }
 
+  function handleClick(name) {
+    let date = new Date();
+
+    let hours = date.getHours();
+
+    if (hours >= 4 && hours < 12) {
+      console.log(name + ", good morning");
+    } else if (hours >= 12 && hours < 17) {
+      console.log(name + ", good afternoon");
+    } else if (hours >= 17 && hours < 20) {
+      console.log(name + ", good evening");
+    } else {
+      console.log(name + ", good night");
+    }
+  }
+
   return (
     <div className="div__button">
-      <button onClick={handleClick}> Add item</button>
+      <button onClick={() => handleClick("Rakshya")}> Add item</button>
       {listArrayItems}
     </div>
   );
