ea9aa39c0cc215c7bc96d09aa2a5971d62b734a9
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-10 08:21:12 +0545

    commit

--- non-static-web/src/App.js
+++ non-static-web/src/App.js
@@ -2,9 +2,20 @@ import "./index.css";
 
 function App() {
   const thingsArray = ["Thing 1", "Thing 2"];
+  const listArrayItems = thingsArray.map((item) => {
+    return <p key={item}>{item}</p>;
+  });
+
+  function handleClick() {
+    const value = thingsArray.length;
+    thingsArray.push("Thing " + (value + 1));
+    console.log(thingsArray);
+  }
+
   return (
-    <div>
-      <button> Add item</button>
+    <div className="div__button">
+      <button onClick={handleClick}> Add item</button>
+      {listArrayItems}
     </div>
   );
 }
