d593629f4b3c9c77ab0453c6d7edfc837ecd6749
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-10 21:42:08 +0545

    commit

--- states-reactapp/src/App.js
+++ states-reactapp/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  let initalNum = 0;
+
+  let [changeNum, setChangeNum] = useState(initalNum);
+
+  function handleMinus() {
+    setChangeNum(changeNum - 1);
+  }
+
+  function handlePlus() {
+    setChangeNum(changeNum + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <button className="button counter--minus" onClick={handleMinus}>
+        -
+      </button>
+      <div className="counter--display">
+        <h1>{changeNum}</h1>
+      </div>
+      <button className="button counter--plus" onClick={handlePlus}>
+        +
+      </button>
     </div>
   );
 }
