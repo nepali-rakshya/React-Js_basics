8b29002ffb971c7dfb50067e049027736ae8e9ba
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-11 17:50:09 +0545

    commit

--- states-reactapp/src/App.js
+++ states-reactapp/src/App.js
@@ -7,11 +7,11 @@ function App() {
   let [changeNum, setChangeNum] = useState(initalNum);
 
   function handleMinus() {
-    setChangeNum((prevValue) => prevValue - 1);
+    setChangeNum((prevChangeNum) => prevChangeNum - 1);
   }
 
   function handlePlus() {
-    setChangeNum((prevValue) => prevValue + 1);
+    setChangeNum((prevChangeNum) => prevChangeNum + 1);
   }
 
   return (
