bab933ac01425baf2a53ab89e110ab9930d3e494
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-11 20:44:33 +0545

    commit

--- non-static-web/src/App.js
+++ non-static-web/src/App.js
@@ -2,53 +2,72 @@ import "./index.css";
 import React, { useState } from "react";
 
 function App() {
-  // const thingsArray = ["Thing 1", "Thing 2"];
-  // const listArrayItems = thingsArray.map((item) => {
-  //   return <p key={item}>{item}</p>;
-  // });
-
-  // function handleClick() {
-  //   const value = thingsArray.length;
-  //   thingsArray.push("Thing " + (value + 1));
-  //   console.log(thingsArray);
-  // }
-
-  // function handleClick(name) {
-  //   let date = new Date();
-
-  //   let hours = date.getHours();
-
-  //   if (hours >= 4 && hours < 12) {
-  //     console.log(name + ", good morning");
-  //   } else if (hours >= 12 && hours < 17) {
-  //     console.log(name + ", good afternoon");
-  //   } else if (hours >= 17 && hours < 20) {
-  //     console.log(name + ", good evening");
-  //   } else {
-  //     console.log(name + ", good night");
-  //   }
-  // }
-
-  let array = ["hello", "rakshya", "cutie"];
-
-  let [isImportant, setIsImportant] = React.useState(array[0]);
-
-  let count = 1;
-  function pushingArray() {
-    if (count < array.length) {
-      let value = array[count];
-      setIsImportant(value);
-      count++;
-    }
-  }
-
   return (
-    <div className="div__button">
-      <button onClick={pushingArray}> Add item</button>
-      {/* {listArrayItems} */}
-      <p>{isImportant}</p>
+    <div className="state">
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div className="state--value">
+        <h1>yes</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//
+//
+//
+//
+//
+
+// function App() {
+// const thingsArray = ["Thing 1", "Thing 2"];
+// const listArrayItems = thingsArray.map((item) => {
+//   return <p key={item}>{item}</p>;
+// });
+
+// function handleClick() {
+//   const value = thingsArray.length;
+//   thingsArray.push("Thing " + (value + 1));
+//   console.log(thingsArray);
+// }
+
+// function handleClick(name) {
+//   let date = new Date();
+
+//   let hours = date.getHours();
+
+//   if (hours >= 4 && hours < 12) {
+//     console.log(name + ", good morning");
+//   } else if (hours >= 12 && hours < 17) {
+//     console.log(name + ", good afternoon");
+//   } else if (hours >= 17 && hours < 20) {
+//     console.log(name + ", good evening");
+//   } else {
+//     console.log(name + ", good night");
+//   }
+// }
+
+// let array = ["hello", "rakshya", "cutie"];
+
+// let [isImportant, setIsImportant] = React.useState(array[0]);
+
+// let count = 1;
+// function pushingArray() {
+//   if (count < array.length) {
+//     let value = array[count];
+//     setIsImportant(value);
+//     count++;
+//   }
+// }
+
+//   return (
+//     <div className="div__button">
+//       <button onClick={pushingArray}> Add item</button>
+//       {/* {listArrayItems} */}
+//       <p>{isImportant}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
