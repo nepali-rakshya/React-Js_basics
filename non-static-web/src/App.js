4c0b7e828bb528ddaea67b643971aa4324c639a1
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-12 14:26:52 +0545

    commit

--- non-static-web/src/App.js
+++ non-static-web/src/App.js
@@ -2,51 +2,22 @@ import "./index.css";
 import React, { useState } from "react";
 
 function App() {
-  const thingsArray = ["Thing 1", "Thing 2"];
-  const listArrayItems = thingsArray.map((item) => {
-    return <p key={item}>{item}</p>;
-  });
+  // const thingsArray = ["Thing 1", "Thing 2", "Thing 3"];
+  const [arrayThings, setArrayThings] = useState(["Thing 1", "Thing 2"]);
 
   function handleClick() {
-    const value = thingsArray.length;
-    thingsArray.push("Thing " + (value + 1));
-    console.log(thingsArray);
+    setArrayThings((prevState) => [
+      ...prevState,
+      `Thing ${prevState.length + 1}`,
+    ]);
   }
 
-  function handleClick(name) {
-    let date = new Date();
-
-    let hours = date.getHours();
-
-    if (hours >= 4 && hours < 12) {
-      console.log(name + ", good morning");
-    } else if (hours >= 12 && hours < 17) {
-      console.log(name + ", good afternoon");
-    } else if (hours >= 17 && hours < 20) {
-      console.log(name + ", good evening");
-    } else {
-      console.log(name + ", good night");
-    }
-  }
-
-  let array = ["hello", "rakshya", "cutie"];
-
-  let [isImportant, setIsImportant] = React.useState(array[0]);
-
-  let count = 1;
-  function pushingArray() {
-    if (count < array.length) {
-      let value = array[count];
-      setIsImportant(value);
-      count++;
-    }
-  }
+  const arrayElements = arrayThings?.map((item) => <p key={item}>{item}</p>);
 
   return (
     <div className="div__button">
-      <button onClick={pushingArray}> Add item</button>
-      {/* {listArrayItems} */}
-      <p>{isImportant}</p>
+      <button onClick={handleClick}> Add item</button>
+      {arrayElements}
     </div>
   );
 }
@@ -89,3 +60,19 @@ export default App;
 // }
 
 // export default App;
+
+// function handleClick(name) {
+//   let date = new Date();
+
+//   let hours = date.getHours();
+
+//   if (hours >= 4 && hours < 12) {
+//     console.log(name + ", good morning");
+//   } else if (hours >= 12 && hours < 17) {
+//     console.log(name + ", good afternoon");
+//   } else if (hours >= 17 && hours < 20) {
+//     console.log(name + ", good evening");
+//   } else {
+//     console.log(name + ", good night");
+//   }
+// }
