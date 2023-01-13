a5a4dafa15200d61b283c53b7ea3d7e1a86c63c9
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-13 21:05:54 +0545

    commit

--- spread-operator/src/App.js
+++ spread-operator/src/App.js
@@ -1,7 +1,29 @@
 import "./index.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  let [bioData, setBioData] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    number: 9863215562,
+    email: "johndoe@gmail.com",
+  });
+
+  let { firstName, lastName, number, email } = bioData;
+
+  return (
+    <div className="App">
+      <div className="bio__img">
+        <img src="/img/avatar.png" alt="avatar" width="200px" /> <br />
+        <img src="/img/star.png" alt="star" />
+      </div>
+      <div>
+        <h1>{`${firstName} ${lastName}`}</h1>
+        <p>{number}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
