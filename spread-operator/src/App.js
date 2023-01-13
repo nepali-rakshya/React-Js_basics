f92d276f7f60669eadfafa56f6b3beabf52a1ee5
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-13 22:20:00 +0545

    commit

--- spread-operator/src/App.js
+++ spread-operator/src/App.js
@@ -7,14 +7,25 @@ function App() {
     lastName: "Doe",
     number: 9863215562,
     email: "johndoe@gmail.com",
+    isFavorite: false,
   });
+
+  function handleClick() {
+    return isFavorite === false ? 
+  }
 
   let { firstName, lastName, number, email } = bioData;
 
   return (
     <div className="App">
       <div className="bio__img">
-        <img src="/img/avatar.png" alt="avatar" width="200px" /> <br />
+        <img src="/img/avatar.png" alt="avatar" width="200px" onClick={handleClick}/>
+        <img
+          src="/img/star-filled.png"
+          alt="star-filled"
+          style={{ display: "none" }}
+        />
+        <br />
         <img src="/img/star.png" alt="star" />
       </div>
       <div>
