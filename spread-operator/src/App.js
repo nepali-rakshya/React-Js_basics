5559cf5380027ef2ddbb4c07084299c5914fd5da
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-14 08:32:51 +0545

    commit

--- spread-operator/src/App.js
+++ spread-operator/src/App.js
@@ -10,23 +10,19 @@ function App() {
     isFavorite: false,
   });
 
-  function handleClick() {
-    return isFavorite === false ? 
-  }
-
   let { firstName, lastName, number, email } = bioData;
 
   return (
     <div className="App">
       <div className="bio__img">
-        <img src="/img/avatar.png" alt="avatar" width="200px" onClick={handleClick}/>
+        <img src="/img/avatar.png" alt="avatar" width="200px" /> <br />
         <img
           src="/img/star-filled.png"
           alt="star-filled"
-          style={{ display: "none" }}
+          // style={{ display: "none" }}
         />
         <br />
-        <img src="/img/star.png" alt="star" />
+        {/* <img src="/img/star.png" alt="star" /> */}
       </div>
       <div>
         <h1>{`${firstName} ${lastName}`}</h1>
