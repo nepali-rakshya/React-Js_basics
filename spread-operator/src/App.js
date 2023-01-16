7df0f49f430f770322e8c31928a08d33d0260f21
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-16 21:31:26 +0545

    commit

--- spread-operator/src/App.js
+++ spread-operator/src/App.js
@@ -1,15 +1,78 @@
 import React, { useState } from "react";
 
 const App = () => {
-  let [objUpdate, setObjUpdate] = useState({
-    name: "rakshya",
-  });
-
-  const handleClick = () => {
-    setObjUpdate((prevState) => (prevState = { ...prevState, name: "ram" }));
-  };
-
-  return <button onClick={handleClick}>{objUpdate.name}</button>;
+  let [value, setValue] = useState("rakshya");
+  return (
+    <div>
+      {value} <br />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default App;
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// let [objUpdate, setObjUpdate] = useState({
+//   name: "rakshya",
+//   condition: true,
+// });
+
+// let { name, condition } = objUpdate;
+
+// const handleClick = () => {
+//   if (objUpdate.condition === true) {
+//     setObjUpdate((prevStat) => ({
+//       ...prevStat,
+//       name: "ram",
+//       condition: false,
+//     }));
+//   } else if (condition === false) {
+//     setObjUpdate((prevStat) => ({
+//       ...prevStat,
+//       name: "rakshya",
+//       condition: true,
+//     }));
+//   }
+// };
+
+// let [update, setUpdate] = useState("rakshya");
+
+// function handleClick() {
+//   update === "rakshya" ? setUpdate("ram") : setUpdate("rakshya");
+// }
+
+// return <button onClick={handleClick}>{update}</button>;
